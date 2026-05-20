f61f48895431674ff033618ed921132db263a3cd
Deepak kr <gowdakrdeepak@gmail.com>
2026-05-20 20:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 67 MB (77.50%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
         int w=arr[0],c=0,n=arr.size();
+         if(k>=n)
+            return *max_element(arr.begin(),arr.end());
         for(int i=1;i<n;i++) {
             if(w>arr[i])
                 c++;
