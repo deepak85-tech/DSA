882f1a3c75c589ed59b95e4688e045e704406058
Deepak kr <gowdakrdeepak@gmail.com>
2026-07-16 01:39:18 +0530

    Time: 8 ms (7.31%) | Memory: 12.3 MB (5.00%) - LeetSync

---
+++ 3353-existence-of-a-substring-in-a-string-and-its-reverse/existence-of-a-substring-in-a-string-and-its-reverse.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isSubstringPresent(string s) {
+        string ss=s;
+        reverse(ss.begin(),ss.end());
+        unordered_map<string,int>m;
+        unordered_map<string,int>mm;
+        int n=s.size();
+        if(n==1) return false;
+        for(int i=0;i<n-1;i++)
+        {
+            string t = s.substr(i, 2);
+            m[t]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            string t = ss.substr(i, 2);
+            mm[t]++;
+        }
+        for(auto &i:m)
+        {
+            if(mm.find(i.first)!=mm.end())
+                return true;
+        }
+        return false;
+        
+    }
+
+};
