0d067beca73d7bcf1a0c64ce3976df84e91c1f15
Deepak kr <gowdakrdeepak@gmail.com>
2026-07-17 11:20:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (37.59%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,7 +4,7 @@ public:
         int n=s.size();
         string t,res;
         int i=0,j=n-1;
-        if(s==" "|| n==1)return true;
+        
         while(i<j)
         {
             if(!isalnum(s[i]))
